0c95e7353cf1e1628bbe9d56e7e8804e56825efd
Oliver Pinel <oliver.debankle@gmail.com>
2019-01-27 11:09:06 +1000

    Working test program

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -1,16 +1,35 @@
 #include "../include/Colors.hpp"
 #include "../include/Compiler.hpp"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << Color::FG_MAGENTA << "Test 1: tests/init_test.func" << endl;
-    cout << Color::FG_MAGENTA << "Compiling init_test" << Color::FG_DEFAULT << endl;
-    system("./bin/func -f tests/init_test.func -o tests/init_test > /dev/null");
-    cout << Color::FG_MAGENTA << "\nTesting init_test" << Color::FG_DEFAULT << endl;
-    system("./tests/init_test");
+void test(std::string test_file, std::string output_file) {
+    cout << Color::FG_MAGENTA << "Test 1: " << test_file << endl;
+    cout << Color::FG_MAGENTA << "Compiling " << test_file << Color::FG_DEFAULT << endl;
+
+    std::string compile_command = "../bin/func -f " + test_file + " -o " + output_file;
+    system(compile_command.c_str());
+
+    cout << Color::FG_MAGENTA << "\nTesting " << test_file << Color::FG_DEFAULT << endl;
+    std::string test_command = "./" + output_file;
+    system(test_command.c_str());
+
     cout << Color::FG_GREEN << "\nTest 1 init_test completed\n" << endl;
+    
+    std::string delete_command = "rm -rf " + output_file;
+    system(delete_command.c_str());
+}
+
+int main() {
+    test("init_test.func", "init_test");
+    test("add_test.func", "add_test");
+    test("sub_test.func", "sub_test");
+    test("mul_test.func", "mul_test");
+    test("set_test.func", "set_test");
+    test("bool_test.func", "bool_test");
+    test("out_test.func", "out_test");
 
     return 0;
 }
